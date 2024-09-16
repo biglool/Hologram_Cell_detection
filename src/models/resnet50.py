3ede146af4e501d62b31425fb540e73d1fa88b7c
Oriol Caravaca <biglool@gmail.com>
2024-09-16 15:27:16 +0200

    Update resnet50.py

--- src/models/resnet50.py
+++ src/models/resnet50.py
@@ -8,7 +8,7 @@ def resnet_segmentation_model(input_shape=(512, 512, 1)):
 
 
     inputs = Input(input_shape)
-    dropout= 0.5
+    dropout= 0.2
 
     #x = Lambda(lambda x: tf.repeat(x, repeats=3, axis=-1))(inputs)
     x = Conv2D(3, (1, 1), padding='same',use_bias=False, activation='relu')(inputs)
